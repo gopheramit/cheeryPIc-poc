adc1a4a2776e239231468ab2f275780fa6eb0c24
goopheramit <amitwpu@gmail.com>
2024-01-15 19:15:29 +0530

    clearn

--- main.go
+++ main.go
@@ -5,8 +5,4 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World from master branch")
 
-	fmt.Println("Hello World from feature branch commit 1")
-
-	fmt.Println("Hello World from feature branch commit 2")
-
 }
